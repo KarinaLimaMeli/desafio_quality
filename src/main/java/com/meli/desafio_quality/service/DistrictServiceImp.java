9774d7d4e82757fd62bad36b28a4e29c2ce64d83
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-13 18:52:19 -0400

    feat: cria a implementacao da interface do DistrictService

---
+++ src/main/java/com/meli/desafio_quality/service/DistrictServiceImp.java
@@ -0,0 +1,17 @@
+package com.meli.desafio_quality.service;
+
+import com.meli.desafio_quality.model.District;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DistrictServiceImp implements DistrictService {
+    @Override
+    public void createDistrict(District district) {
+
+    }
+
+    @Override
+    public District getDistrictByName(String name) {
+        return null;
+    }
+}
