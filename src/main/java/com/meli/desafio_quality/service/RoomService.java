0c17a5b908813d5c2610770eac69cce8fd75dc88
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-13 17:04:57 -0300

    feat: implementa interface RoomService

---
+++ src/main/java/com/meli/desafio_quality/service/RoomService.java
@@ -0,0 +1,8 @@
+package com.meli.desafio_quality.service;
+
+import com.meli.desafio_quality.model.Room;
+
+public interface RoomService {
+    Room calculateRoomArea();
+    Room getBiggestRoom();
+}
---
+++ src/main/java/com/meli/desafio_quality/service/RoomServiceImp.java
@@ -0,0 +1,15 @@
+package com.meli.desafio_quality.service;
+
+import com.meli.desafio_quality.model.Room;
+
+public class RoomServiceImp implements RoomService {
+    @Override
+    public Room calculateRoomArea() {
+        return null;
+    }
+
+    @Override
+    public Room getBiggestRoom() {
+        return null;
+    }
+}
