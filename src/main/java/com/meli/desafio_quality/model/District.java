01a7cad0f47a327ee95fbe7afbf691cfc9af32ab
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-14 13:46:20 -0300

    feat: implementa validações no District Model

--- src/main/java/com/meli/desafio_quality/model/District.java
+++ src/main/java/com/meli/desafio_quality/model/District.java
@@ -3,13 +3,23 @@ package com.meli.desafio_quality.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
-@Builder
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class District {
+
+    @NotBlank(message = "O bairro não pode estar vazio.")
+    @Size(max = 45, message = "O comprimento do bairro não pode exceder 45 caracteres.")
     private String districtName;
+
+    @NotBlank(message = "O valor do metro quadrado não pode estar vazio.")
     private BigDecimal valueDistrictM2;
+
 }
