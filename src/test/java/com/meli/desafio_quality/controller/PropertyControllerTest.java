8ffbc6389f3b4ab950d15e456a94bbda3288aaeb
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-14 17:43:47 -0300

    Implementado caso teste de sucesso do createProperty

---
+++ src/test/java/com/meli/desafio_quality/controller/PropertyControllerTest.java
@@ -0,0 +1,61 @@
+package com.meli.desafio_quality.controller;
+
+import com.meli.desafio_quality.model.Property;
+import com.meli.desafio_quality.service.PropertyService;
+import com.meli.desafio_quality.util.UtilProperty;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class PropertyControllerTest {
+
+    @InjectMocks
+    private PropertyController controller;
+
+    @Mock
+    PropertyService propertyService;
+
+    @Test
+    void createProperty() {
+        Property property = UtilProperty.allProperies().get(0);
+
+        ResponseEntity<Void> response = controller.createProperty(property);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNull();
+
+        Mockito.verify(propertyService, Mockito.atLeastOnce()).createProperty(property);
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void calculateTotalArea() {
+    }
+
+    @Test
+    void calculateTotalPrice() {
+    }
+
+    @Test
+    void biggestRoom() {
+    }
+
+    @Test
+    void getAreaByRooms() {
+    }
+}
