d6168b825f23341e16e5be17b355ead80edc5c13
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-14 12:11:12 -0300

    feat: implementa exeptios para District

---
+++ src/main/java/com/meli/desafio_quality/exception/ErrorResponseDetails.java
@@ -0,0 +1,16 @@
+package com.meli.desafio_quality.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponseDetails {
+    private String title;
+    private int status;
+    private String link;
+    private String message;
+    private LocalDateTime timestamp;
+}
