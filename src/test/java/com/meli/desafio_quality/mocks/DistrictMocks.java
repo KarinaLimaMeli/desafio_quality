4887d2ba804f62e3537e145c4db8c4a60728d6a3
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-13 22:39:05 -0300

    feat: implementa sobrecarga de metodo mock_getDistrictByName

--- src/test/java/com/meli/desafio_quality/mocks/DistrictMocks.java
+++ src/test/java/com/meli/desafio_quality/mocks/DistrictMocks.java
@@ -1,4 +1,5 @@
 package com.meli.desafio_quality.mocks;
+import com.meli.desafio_quality.repository.DistrictRepository;
 import com.meli.desafio_quality.service.DistrictService;
 import com.meli.desafio_quality.model.District;
 import com.meli.desafio_quality.util.Util;
@@ -12,6 +13,13 @@ public class DistrictMocks {
         District findedDistrict = Util.allDistricts().stream().filter(
                 district -> district.getDistrictName().equals(name)).collect(Collectors.toList()).get(0);
         BDDMockito.when(service.getDistrictByName(ArgumentMatchers.anyString()))
+                .thenReturn(findedDistrict);
+    }
+
+    public static void mock_getDistrictByName(String name, DistrictRepository repository) {
+        District findedDistrict = Util.allDistricts().stream().filter(
+                district -> district.getDistrictName().equals(name)).collect(Collectors.toList()).get(0);
+        BDDMockito.when(repository.getDistrictByName(ArgumentMatchers.anyString()))
                 .thenReturn(findedDistrict);
     }
 
