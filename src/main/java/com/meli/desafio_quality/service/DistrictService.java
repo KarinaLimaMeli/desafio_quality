6e949a4cbed409b30db85a042a22a2a7a7059f18
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-13 22:42:30 -0300

    feat: implementa teste para a funçao getDistrictByName na camada service

--- src/main/java/com/meli/desafio_quality/service/DistrictService.java
+++ src/main/java/com/meli/desafio_quality/service/DistrictService.java
@@ -5,6 +5,7 @@ import com.meli.desafio_quality.model.District;
 public interface DistrictService {
 
     void createDistrict(District district);
+
     District getDistrictByName(String name);
 
 }
