493952243c66fef7827e908f33afe81f251e1058
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-15 11:43:52 -0400

    test: mock do repository para DistrictServiceTest

--- src/test/java/com/meli/desafio_quality/mocks/DistrictMocks.java
+++ src/test/java/com/meli/desafio_quality/mocks/DistrictMocks.java
@@ -6,9 +6,6 @@ import com.meli.desafio_quality.util.Util;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 
-import java.util.List;
-import java.util.Optional;
-
 public class DistrictMocks {
     public static void mock_getDistrictByName(String name, DistrictService service) {
         District foundDistrict = Util.allDistricts().stream().filter(
@@ -42,14 +39,20 @@ public class DistrictMocks {
         BDDMockito.when(service.createDistrict(ArgumentMatchers.any(District.class))).
                 thenReturn(district);
     }
-    public static void mock_createDistrict(DistrictRepository repository, District createDistrict, List<District> bd){
-        Optional<District> foundDistrict = bd.stream().filter(district -> district.getDistrictName()
-                .equalsIgnoreCase(createDistrict.getDistrictName())).findFirst();
-        if(foundDistrict.isPresent()){
-            BDDMockito.doThrow(new RuntimeException("teste")).when(repository).createDistrict(ArgumentMatchers.any(District.class));
-        }if(foundDistrict.isEmpty()) {
-            BDDMockito.doNothing().when(repository).createDistrict(ArgumentMatchers.any(District.class));
-        }
+
+//    public static void mock_createDistrict(DistrictRepository repository, District createDistrict, List<District> bd){
+//        Optional<District> foundDistrict = bd.stream().filter(district -> district.getDistrictName()
+//                .equalsIgnoreCase(createDistrict.getDistrictName())).findFirst();
+//        if(foundDistrict.isPresent()){
+//            BDDMockito.doThrow(new RuntimeException("teste")).when(repository).createDistrict(ArgumentMatchers.any(District.class));
+//        }if(foundDistrict.isEmpty()) {
+//            BDDMockito.doNothing().when(repository).createDistrict(ArgumentMatchers.any(District.class));
+//        }
+//    }
+
+    public static void mock_createDistrict(DistrictRepository repository, District district) {
+        BDDMockito.when(repository.createDistrict(ArgumentMatchers.any(District.class))).
+                thenReturn(district);
     }
 
     public static void mock_districtAlreadyExist(DistrictService service) {
@@ -59,4 +62,10 @@ public class DistrictMocks {
                 });
     }
 
+    public static void mock_districtAlreadyExist(DistrictRepository repository) {
+        BDDMockito.when(repository.createDistrict(ArgumentMatchers.any(District.class)))
+                .thenAnswer(invocationOnMock -> {
+                    throw new Exception("teste");
+                });
+    }
 }
