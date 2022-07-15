a798c5886d366b2714a6081ce78856ce828726ce
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-15 11:28:32 -0400

    fix: corrige http status dos metodos

--- src/main/java/com/meli/desafio_quality/handlerException/HandlerDistrictExeptions.java
+++ src/main/java/com/meli/desafio_quality/handlerException/HandlerDistrictExeptions.java
@@ -17,24 +17,24 @@ public class HandlerDistrictExeptions {
     public ResponseEntity<ErrorResponseDetails> handlerErrorNotFound(DistrictNotFound districtNotFound) {
         return new ResponseEntity<>(
                 ErrorResponseDetails.builder()
-                        .title("Bad Request")
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .link("https://http.cat/400")
+                        .title("Not found")
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .link("https://http.cat/404")
                         .message(districtNotFound.getMessage())
                         .timestamp(LocalDateTime.now())
-                        .build(), HttpStatus.BAD_REQUEST);
+                        .build(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DistrictAlreadyExist.class)
     public ResponseEntity<ErrorResponseDetails> handlerErrorNotFound(DistrictAlreadyExist districtAlreadyExist) {
         return new ResponseEntity<>(
                 ErrorResponseDetails.builder()
-                        .title("Not Found")
-                        .status(HttpStatus.NOT_FOUND.value())
-                        .link("https://http.cat/404")
+                        .title("Bad request")
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .link("https://http.cat/400")
                         .message(districtAlreadyExist.getMessage())
                         .timestamp(LocalDateTime.now())
-                        .build(), HttpStatus.NOT_FOUND);
+                        .build(), HttpStatus.BAD_REQUEST);
     }
 
 }
