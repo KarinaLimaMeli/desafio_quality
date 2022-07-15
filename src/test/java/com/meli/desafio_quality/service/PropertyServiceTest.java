707a85715c48dc2b278600ceeaffba5ee7076f38
sandiouriquemeli <sandi.ourique@mercadolivre.com>
2022-07-15 14:29:10 -0300

    teste de validação do calculo da área total

--- src/test/java/com/meli/desafio_quality/service/PropertyServiceTest.java
+++ src/test/java/com/meli/desafio_quality/service/PropertyServiceTest.java
@@ -1,68 +1,77 @@
 package com.meli.desafio_quality.service;
 
 import com.meli.desafio_quality.exception.DistrictNotFound;
+import com.meli.desafio_quality.model.District;
 import com.meli.desafio_quality.model.Property;
 import com.meli.desafio_quality.repository.DistrictRepository;
 import com.meli.desafio_quality.repository.PropertyRepository;
 import com.meli.desafio_quality.util.UtilDistrict;
 import com.meli.desafio_quality.util.UtilProperty;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.atLeastOnce;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@Log4j2
 class PropertyServiceTest {
 
     @InjectMocks
-    PropertyServiceImp propertyServiceImp;
-
-    @Mock
-    DistrictRepository districtRepository;
+    PropertyServiceImp service;
 
     @Mock
     PropertyRepository propertyRepository;
 
+    @Mock
+    DistrictRepository districtRepository;
+
     @BeforeEach
     public void setup() {
         BDDMockito.doNothing().when(propertyRepository).createProperty(ArgumentMatchers.any(Property.class));
-        BDDMockito.when(districtRepository.getDistrictByName(ArgumentMatchers.anyString())).
-                thenReturn(UtilDistrict.allDistricts().get(0));
-        BDDMockito.when(propertyServiceImp.calculateTotalArea(ArgumentMatchers.anyString()))
-                .thenReturn(ArgumentMatchers.anyDouble());
+
+        BDDMockito.when(districtRepository.getDistrictByName(ArgumentMatchers.anyString()))
+            .thenReturn(UtilDistrict.allDistricts().get(0));
+
+        BDDMockito.when(propertyRepository.findByName(ArgumentMatchers.anyString()))
+                .thenReturn(UtilProperty.allProperies().get(0));
     }
 
     @Test
     void createProperty() {
         Property newProperty = UtilProperty.allProperies().get(0);
-        propertyServiceImp.createProperty(newProperty);
-        verify(propertyRepository, atLeastOnce()).createProperty(newProperty);
+        service.createProperty(newProperty);
+
+        verify(propertyRepository, Mockito.atLeastOnce()).createProperty(newProperty);
     }
 
     @Test
     void notCreateProperty() {
         Property newProperty = UtilProperty.propertyDistrictNull();
         try {
-            propertyServiceImp.createProperty(newProperty);
+            service.createProperty(newProperty);
         } catch (DistrictNotFound districtNotFound) {
             assertEquals(districtNotFound.getMessage(), "Bairro não cadastrado!");
         }
     }
 
     @Test
+    @DisplayName("Valida se retorna o valor total da área")
     void calculateTotalArea() {
+        Property newProperty = UtilProperty.allProperies().get(0);
+        service.createProperty(newProperty);
+        String name = newProperty.getPropertyName();
+        double result = service.calculateTotalArea(name);
+
+        assertEquals(result, 150.0);
 
     }
 
