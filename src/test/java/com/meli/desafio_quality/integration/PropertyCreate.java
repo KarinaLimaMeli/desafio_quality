30c1494ace8494530414f88c1a480b720710bee4
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-15 19:47:55 -0300

    Método implementado, porem está com erro no teste (ler comentário explicativo)

--- src/test/java/com/meli/desafio_quality/integration/PropertyCreate.java
+++ src/test/java/com/meli/desafio_quality/integration/PropertyCreate.java
@@ -1,19 +1,26 @@
 package com.meli.desafio_quality.integration;
 
+import com.meli.desafio_quality.model.District;
 import com.meli.desafio_quality.model.Property;
+import com.meli.desafio_quality.util.UtilDistrict;
 import com.meli.desafio_quality.util.UtilProperty;
+
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 @Log4j2
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PropertyCreate {
@@ -32,13 +39,22 @@ public class PropertyCreate {
 
         HttpEntity<Property> httpEntity = new HttpEntity<>(property);
 
-        ResponseEntity<Property> result = testRestTemplate.exchange(
+        ResponseEntity<Void> result = testRestTemplate.exchange(
             baseURL,
             HttpMethod.POST,
             httpEntity,
-            Property.class
+            Void.class
         );
 
-        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        // LOGS PARA IDENTIFICAR O QUE ESTÁ SENDO RETORNADO
+        log.info(httpEntity);
+        log.info(result);
+        log.info(result.getStatusCode());
+        log.info(result.getBody());
+
+        // ️⁉️ DESCOBRIR PORQUE ESTÁ VOLTANDO 404 NOT_FOUND, E NÃO O CREATED
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED); // ❌
+        assertThat(result.getBody()).isNull();
     }
 }
