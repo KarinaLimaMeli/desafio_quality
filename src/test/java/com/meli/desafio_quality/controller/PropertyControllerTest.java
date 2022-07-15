8f45deaea8f475e40f2d5758830bc7aab9c24acd
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-15 10:36:38 -0300

    Implementado o teste do método calculateTotalArea

--- src/test/java/com/meli/desafio_quality/controller/PropertyControllerTest.java
+++ src/test/java/com/meli/desafio_quality/controller/PropertyControllerTest.java
@@ -3,6 +3,7 @@ package com.meli.desafio_quality.controller;
 import com.meli.desafio_quality.model.Property;
 import com.meli.desafio_quality.service.PropertyService;
 import com.meli.desafio_quality.util.UtilProperty;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@Log4j2
 class PropertyControllerTest {
 
     @InjectMocks
@@ -30,6 +32,12 @@ class PropertyControllerTest {
     @BeforeEach
     public void setup() {
         BDDMockito.doNothing().when(propertyService).createProperty(ArgumentMatchers.any(Property.class));
+
+        BDDMockito.when(propertyService.calculateTotalArea(ArgumentMatchers.anyString()))
+            .thenReturn(150.0);
+
+//        BDDMockito.when(propertyService.findByName(ArgumentMatchers.anyString()))
+//            .thenReturn(UtilProperty.allProperies().get(0));
     }
 
     @Test
@@ -46,11 +54,37 @@ class PropertyControllerTest {
     }
 
     @Test
+    @DisplayName("Valida se retorna o status 400 quando não são informados os dados corretos de uma propriedade.")
+    void createProperty_returnHttpCBadRequest_whenNewPropertyWithoutDistrict() {
+        // Property property = UtilProperty.propertyDistrictNull();
+        Property property = new Property();
+        property.setPropertyName("propertyName");
+        property.setRoomList(UtilProperty.allRooms());
+        ResponseEntity<Void> response = controller.createProperty(property);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody()).isNull();
+
+        Mockito.verify(propertyService, Mockito.atLeastOnce()).createProperty(property);
+    }
+
+    @Test
     void findByName() {
     }
 
     @Test
     void calculateTotalArea() {
+        // CRIA O PROPRIEDADE
+        Property property = UtilProperty.allProperies().get(0);
+        // log.info(property);
+        ResponseEntity<Void> result = controller.createProperty(property);
+
+        String name = property.getPropertyName();
+        log.info(name);
+        ResponseEntity<Double> response = controller.calculateTotalArea(name);
+
+        assertThat(response.getBody()).isPositive();
+        assertThat(response.getBody()).isEqualTo(150.0);
     }
 
     @Test
